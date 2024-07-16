86b2d198d971c080380e4221b470a549e7fab6dc
rahatfaruk <rahatfaruk99@gmail.com>
2024-07-16 19:24:44 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
 
 // vars
 const app = express()
 const port = process.env.PORT || 5000
+const uriMDB = `mongodb+srv://${process.env.UserMDB}:${process.env.PasswordMDB}@cluster0.ympa4ek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+const client = new MongoClient(uriMDB, {
+  serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true,}
+})
 
 // middleware
 app.use( cors() )
 app.use( express.json() )
 
-app.get('/', (_, res) => res.send('Welcome to backend!'))
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    // ## req-group: get
+    app.get('/', (_, res) => res.send('Welcome to backend!'))
+
+    // check successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+run().catch(console.dir);
+
+// listen to the server
 app.listen(port, () => console.log('listening from server!'))
